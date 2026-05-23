ecdc06507dadc2386de45b9399b0fcfa8a7aba6b
JuanBernardo <uribejuanb@javeriana.edu.co>
2026-05-22 19:02:29 -0500

    add: loader.py spearación de responsabilidades, espera los datos verifica que no haya nulos, verifica las fechas, melo mejor dicho

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import src.data.loader as loader
+from pathlib import Path
 
 
 def main():
-
-    csvOriginal = "Steel_industry_data.csv"
-    csvProbarQueFunciona = "prueba.csv"
-
-    dataFrame = loader.leerCSV(csvProbarQueFunciona, "processed")
-
-    print(f"Dataframe:\n {dataFrame}")
-
-    return
-
+    
+    df = loader.leer_csv(Path("data/raw/Steel_industry_data.csv"))
+    print(f"Shape: {df.shape}")
+    print(f"Dtypes:\n{df.dtypes}")
+    print(f"Rango fechas: {df['date'].min()} → {df['date'].max()}")
+    print(df.head())
 
 
 if __name__ == "__main__":
--- src/data/loader.py
+++ src/data/loader.py
@@ -1,27 +1,71 @@
-import sys
-from pathlib import Path
-
-RAIZ = Path(__file__).parent.parent.parent
-sys.path.append(str(RAIZ))
-
 import pandas as pd
 
-def leerCSV(nombreCSV, rutaCarpeta) :
-
-    rutaArchivo = RAIZ / "data" / rutaCarpeta / nombreCSV
+def leer_csv(rutaArchivo) :
 
     #Todas las columnas : date,Usage_kWh,Lagging_Current_Reactive.Power_kVarh,Leading_Current_Reactive_Power_kVarh,CO2(tCO2),Lagging_Current_Power_Factor,Leading_Current_Power_Factor,NSM,WeekStatus,Day_of_week,Load_Type
+    columnasEsperadas = ["date",
+                         "Usage_kWh",
+                         "Lagging_Current_Reactive.Power_kVarh",
+                         "Leading_Current_Reactive_Power_kVarh",
+                         "CO2(tCO2)",
+                         "Lagging_Current_Power_Factor",
+                         "Leading_Current_Power_Factor",
+                         "NSM",
+                         "WeekStatus",
+                         "Day_of_week",
+                         "Load_Type"]
 
-    columnasUtilizar = ["Lagging_Current_Reactive.Power_kVarh",
-                        "Leading_Current_Reactive_Power_kVarh", 
-                        "Lagging_Current_Power_Factor",
-                        "Leading_Current_Power_Factor",
-                        "NSM",
-                        "WeekStatus",
-                        "Day_of_week",
-                        "Load_Type"
-]
-    dataFrame = pd.read_csv(rutaArchivo, usecols = columnasUtilizar)
+    dataFrameCrudo = pd.read_csv(rutaArchivo)
+
+    validarSchema(dataFrameCrudo, columnasEsperadas)
+
+    dataFrame = parseDates(dataFrameCrudo, "date")
+
+    dataFrame = dataFrame.sort_values("date").reset_index(drop = True)
+
+    errores = dataFrame.isnull().sum().sum()
+
+    if(errores > 0) :
+        print(f"hay {errores} cantidad de nulos")
+        return 
+    
+    timestampsDup = dataFrame["date"].duplicated().sum()
+
+    if(timestampsDup > 0) :
+        print(f"hay {timestampsDup} dates duplicados")
 
     return dataFrame
 
+def validarSchema(dataFrameCrudo, columnasEsperadas) :
+    set_actual = set(dataFrameCrudo.columns)
+    set_esperado = set(columnasEsperadas)
+
+    if set_actual != set_esperado :
+
+        faltan = set_esperado - set_actual
+        sobran = set_actual - set_esperado
+
+        raise ValueError(
+            f"[Loader] Mira, aquí hay información útil:\n "
+            f"[Loader] los que hay: {set_actual}\n "
+            f"[Loader] Mmmm al parecer hay un error con las columnas esperadas y lo que lees\n "
+            f"[Loader] Faltan (set_esperado - set_actual) : {faltan}\n "
+            f"[Loader] Sobran (set_actual - set_esperado) : {sobran}"
+        )
+    
+    return 
+
+def parseDates(dataFrameCrudo, colDate = "date") :
+    try:
+        dfCopia = dataFrameCrudo.copy()
+
+        dfCopia[colDate] = pd.to_datetime(dfCopia[colDate], dayfirst = True, errors = "raise")
+
+        return dfCopia
+
+    except Exception as e :
+        raise ValueError(
+            f"[Loader] Error al convertir fechas"
+            f"[Loader] mensaje del error :\n{e}"
+            ) from e
+    
